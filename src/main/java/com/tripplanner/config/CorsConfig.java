773ddb90229fafc72c90d2394de34bb14dc90a3c
ayushrtripathi <ayushrt108@gmail.com>
2025-09-21 13:30:56 +0530

    Add CorsConfig

--- src/main/java/com/tripplanner/config/CorsConfig.java
+++ src/main/java/com/tripplanner/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
